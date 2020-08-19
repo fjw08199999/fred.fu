0b48b2298091ecffe4bf21ae922fce53158ab11b
FredFu <fjw08199999@gmail.com>
2020-08-19 10:06:08 +0800

    新建案

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlueTooth_Test_01
+//
+//  Created by fred fu on 2020/8/18.
+//  Copyright © 2020 fred fu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
